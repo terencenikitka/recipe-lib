888e111d8bc006e37fe24314183c5d5c26b533b5
Peyton Meredith <peymeredith@gmail.com>
2023-11-27 14:26:43 -0600

    removed redundant imports

--- server/app.py
+++ server/app.py
@@ -3,10 +3,8 @@
 # Standard library imports
 
 # Remote library imports
-from flask import Flask, request, make_response, jsonify
-from flask_cors import CORS
-from flask_restful import Api, Resource
-from flask_migrate import Migrate
+from flask import request, make_response, jsonify
+from flask_restful import Resource
 import os
 
 # Local imports
@@ -16,18 +14,10 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATABASE = os.environ.get(
     "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
-app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-
-CORS(app)
-
-migrate = Migrate(app, db, render_as_batch=True)
-
-db.init_app(app)
-
-api = Api(app)
 
 
 @app.route('/')
