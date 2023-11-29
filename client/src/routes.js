aab134afe4a74de69983f5a52c70e03da47d50cc
Nikita <terencekity@gmail.com>
2023-11-29 16:34:50 -0500

    made RecipeCardPage

--- client/src/routes.js
+++ client/src/routes.js
@@ -6,7 +6,8 @@ import Profile from "./components/Profile";
 import Recipes from "./components/Recipes";
 import Search from "./components/Search";
 import Signup from "./components/Signup";
-
+import RecipeCardPage from "./components/RecipeCardPage";
+import RecipeCard from "./components/RecipeCard";
 
 const routes = [
     {
@@ -37,6 +38,10 @@ const routes = [
             {
                 path: "/signup",
                 element: <Signup />
+            },
+            {
+                path:`/recipes/:id`,
+                element: <RecipeCardPage />
             }
         ]
     }
