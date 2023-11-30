5d6ca84b1af35a79315695b0c19943f63ef4497b
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-11-29 21:41:01 -0600

    small changes

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { useOutletContext } from "react-router-dom";
+import { useOutletContext,useNavigate } from "react-router-dom";
 
 function Login(){
-
+    const navigate = useNavigate()
     const login = useOutletContext()
     const [formData, setFormData] = useState({
         email: '',
@@ -22,14 +22,14 @@ function Login(){
     }
 
     return (
-        <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-            <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-                <div class="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+        <div class="font-mono min-h-screen bg-gray-400 py-6 flex flex-col justify-center sm:py-12 ">
+            <div class="relative py-3 sm:max-w-xl sm:mx-auto" >
+                <div class="container mx-auto bg-primary text-primary w-md absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl ">
                 </div>
-                <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+                <div class="container mx-auto bg-secondary text-primary w-md relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
                     <div class="max-w-md mx-auto">
                         <div>
-                            <h1 class="text-2xl font-semibold">Login Form with Floating Labels</h1>
+                            <h1 class="text-2xl font-semibold">Login to your Account</h1>
                         </div>
                         <div class="divide-y divide-gray-200">
                             <form onSubmit={handleLogin}>
@@ -60,8 +60,17 @@ function Login(){
                                         />
                                         <label for="password" class="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
                                     </div>
+                                    <div className="text-right">
+                                        <button className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800 mt-1.5 underline" onClick={() => navigate('/login')/*navigate to /forgot-password*/}>Forgot your password?</button>
+                                    </div>
                                     <div class="relative">
-                                        <button type="submit" class="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
+                                        <button type="submit" class="btn-primary btn w-full px-4 py-2 font-bold text-white bg-blue-400 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline" onClick={() => navigate('/profile')}>Sign In</button>
+                                        <hr className="mb-6 border-t" />
+
+                                        <div className="text-center">
+                                            <p>Don't have an account?</p>
+                                            <button className="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800 mt-1.5 underline" onClick={() => navigate('/login')}>Create account!</button>
+                                        </div>
                                     </div>
                                 </div>
                             </form>
