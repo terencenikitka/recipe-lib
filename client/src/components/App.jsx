29b0bca502888407bf68a765876c4cc45628da3b
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-12-01 04:17:30 -0600

    fixed some issues

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route, Outlet, useNavigate, Navigate } from "react-router-dom";
+import { Switch, Route, Outlet, useNavigate, Navigate, useLocation } from "react-router-dom";
 import NavBar from "./NavBar";
-import Home from "./Home";
+import Recipes from "./Recipes";
 import "../index.css"
 
 function App() {
@@ -9,7 +9,9 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
   const [showLogoutAlert, setShowLogoutAlert] = useState(false)
+  const [chefs, setChefs] = useState([])
   const navigate = useNavigate()
+  const location = useLocation()
 
   const login = () => {
     setIsLoggedIn(true)
@@ -23,51 +25,68 @@ function App() {
     setUser
   }
   useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/", {replace: true})
+    }}, [isLoggedIn])
+
+  useEffect(() => {
+      fetch("http://127.0.0.1:5555/chefs")
+          .then( r => {
+              if (r.ok) {
+                  return r.json()
+              }
+              throw r
+          })
+          .then((chefsData) => {
+              setChefs(chefsData)
+              console.log('Chefs Data:', chefsData)
+          })
+          .catch((e) => {
+              console.error("Error fetching chefs:", e)
+          })
+  }, [])
+  useEffect(() => {
     fetch("/check_session").then((r) => {
       if (r.ok) {
         r.json().then((user) => setUser(user));
       }
     });
-    if (isLoggedIn) {
-      navigate("/", {replace: true})
-    }}, [isLoggedIn])
-
-
-  return ( 
-    <div className="bg-base-100 flex">
-      <header>
-        <NavBar isLoggedIn={isLoggedIn} logout={logout} login={login} />  
-      </header>
-      {showLogoutAlert && (
-        <div role="alert" className={`alert alert-success custom-alert ${showLogoutAlert ? 'fadeout' : 'fadein'}`} onAnimationEnd={() => setShowLogoutAlert(false)} style={{display: showLogoutAlert ? 'block': 'none'}}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="stroke-current shrink-0 h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-          <span>You have been successfully logged out!</span>
+    })
+    
+    return ( 
+      <div className="bg-base-100 flex">
+        <header className="w-full h-10">
+          <NavBar isLoggedIn={isLoggedIn} logout={logout} login={login} />  
+        </header>
+        
+        {showLogoutAlert && (
+          <div role="alert" className={`alert alert-success custom-alert ${showLogoutAlert ? 'fadeout' : 'fadein'}`} onAnimationEnd={() => setShowLogoutAlert(false)} style={{display: showLogoutAlert ? 'block': 'none'}}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="stroke-current shrink-0 h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>You have been successfully logged out!</span>
+          </div>
+        )}
+        
+        
+        <div className="flex-1">
+          <Outlet context={context}/>
+          {(location.pathname === "/" ) && <Recipes />}
+          
         </div>
-      )}
-      <div className="flex-1">
-        <Outlet context={context}/>
       </div>
       
-      
-    </div>
-    
-);
-}
-
-export default App;
-
-
-
-// w-max mx-auto alert-box transition-opacity duration-1000 ease-in-out opacity-100
+  );
+  }
+  
+  export default App;
